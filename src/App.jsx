9709786e1733100bc3f14a1c854aede6faf15491
Kenny <adelekekehinde06@gmail.com>
2024-09-21 02:23:11 +0100

    feat: a bunch of new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
+import { Box, Flex, Spinner, Text } from "@radix-ui/themes";
 import "./App.css";
-import TaskForm from "./components/TaskForm";
 import TaskColumn from "./components/TaskColumn";
-import todoIcon from "./assets/direct-hit.png";
-import doingIcon from "./assets/glowing-star.png";
-import doneIcon from "./assets/check-mark-button.png";
-import { addMoodAPI, getMoodsAPI, updateMoodAPI } from "./services/apiService";
-const oldTasks = localStorage.getItem("tasks");
+import TaskForm from "./components/TaskForm";
+import { getMoodsAPI, updateMoodAPI } from "./services/apiService";
+
+// you read the env by adding VITE as the prefix
+const backendURL = import.meta.env.VITE_BACKEND_URL;
+
+console.log({ backendURL });
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -15,7 +17,7 @@ const App = () => {
   const [token, setToken] = useState([]);
   const onDrop = async (status, position) => {
     console.log(
-      `${activeCard} is going to ${status} and in position ${position}`
+      `${activeCard} is going to ${status} and in position ${position}`,
     );
 
     if (activeCard == null || activeCard === undefined) return;
@@ -38,7 +40,7 @@ const App = () => {
       await updateMoodAPI(taskToMove.id, status);
       // Optionally handle success (e.g., show a notification)
     } catch (error) {
-      console.error('Error updating task on the server:', error);
+      console.error("Error updating task on the server:", error);
       // Rollback the local state if the update fails
       fetchTasks(); // Re-fetch tasks from the server
     }
@@ -56,47 +58,58 @@ const App = () => {
     }
   };
   useEffect(() => {
-
     fetchTasks(); // Call the function to fetch tasks
-  }, []); 
-
-  
-  
-
- 
-
-  
+  }, []);
 
   if (loading) {
-    return <p>LOADING!</p>;
+    return (
+      <Box
+        style={{
+          display: "grid",
+          placeItems: "center",
+        }}
+        width={"100%"}
+        height={"100vh"}
+      >
+        <Flex align={"center"} gap={"2"}>
+          <Spinner />
+          <Text>
+            Loading{" "}
+            <Text weight={"medium"} color="purple">
+              Tasks...
+            </Text>
+          </Text>
+        </Flex>
+      </Box>
+    );
   }
   return (
     <div className="app">
-      { loading === false && (
-        <div>
+      {loading === false && (
+        <Box py={"6"}>
           <TaskForm setTasks={setToken} tasks={token} />
           <main className="app_main">
             <TaskColumn
               title="To do"
-              icon={todoIcon}
+              icon={<ListIcon />}
               tasks={token}
               status="todo"
               setActiveCard={setActiveCard}
               onDrop={onDrop}
               setTasks={setToken}
-              />
+            />
             <TaskColumn
               title="Doing"
-              icon={doingIcon}
+              icon={<Progress />}
               tasks={token}
               status="doing"
               setActiveCard={setActiveCard}
               onDrop={onDrop}
               setTasks={setToken}
-              />
+            />
             <TaskColumn
               title="Done"
-              icon={doneIcon}
+              icon={<Done />}
               tasks={token}
               status="done"
               setActiveCard={setActiveCard}
@@ -104,11 +117,76 @@ const App = () => {
               onDrop={onDrop}
             />
           </main>
-         
-        </div>
+        </Box>
       )}
     </div>
   );
 };
+
+function ListIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect x="3" y="5" width="6" height="6" rx="1" />
+      <path d="m3 17 2 2 4-4" />
+      <path d="M13 6h8" />
+      <path d="M13 12h8" />
+      <path d="M13 18h8" />
+    </svg>
+  );
+}
+
+function Done() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M18 6 7 17l-5-5" />
+      <path d="m22 10-7.5 7.5L13 16" />
+    </svg>
+  );
+}
+
+function Progress() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M10.1 2.182a10 10 0 0 1 3.8 0" />
+      <path d="M13.9 21.818a10 10 0 0 1-3.8 0" />
+      <path d="M17.609 3.721a10 10 0 0 1 2.69 2.7" />
+      <path d="M2.182 13.9a10 10 0 0 1 0-3.8" />
+      <path d="M20.279 17.609a10 10 0 0 1-2.7 2.69" />
+      <path d="M21.818 10.1a10 10 0 0 1 0 3.8" />
+      <path d="M3.721 6.391a10 10 0 0 1 2.7-2.69" />
+      <path d="M6.391 20.279a10 10 0 0 1-2.69-2.7" />
+    </svg>
+  );
+}
 
 export default App;
